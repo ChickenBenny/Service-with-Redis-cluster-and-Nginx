e30b850df39fe8b7ce50c1c8d25bbf01b119db06
ChickenBenny <zxc123benny14159@gmail.com>
2022-07-08 17:39:02 +0800

    complete all the function

--- data/main.py
+++ data/main.py
@@ -10,38 +10,39 @@ class Data(BaseModel):
 app = FastAPI()
 redis = Redis(host='redis', port=6379)
 
-@app.get("/")
+@app.get("/data")
 async def home():
     return {"message": "This is the worker which can manipulate the database."}
 
-@app.get("/cleanup")
+@app.get("/data/cleanup")
 async def cleanup():
-    redis.flushdb()
+    redis.flushall()
     return {"message": "Clean all the data."}
 
-@app.get("/get/{id}")
+@app.get("/data/get/{id}")
 async def input(id: str):
     data = redis.get(id)
     return {"message": data}    
 
-@app.post("/get/")
+@app.post("/data/get/")
 async def input(data: Data):
     redis.set(str(data['key']), str(data['value']))
     return {"message": f"Set KEY : {data['key']} and Value : {data['value']}."}
 
-@app.put("/put/{id}")
+@app.put("/data/put/{id}")
 async def update(id: str, data: Data):
     redis.set(str(data['key']), str(data['value']))
     return {"message": f"Update KEY : {data['key']} and Value : {data['value']}."}    
 
-@app.delete("/delete/{id}")
+@app.delete("/data/delete/{id}")
 async def delete(id: str):
     redis.delete(id)
     return {"message": f"Delete {id}."}
 
-@app.get("/test")
-async def test():
-    data = pd.read_csv('./data')
-    for i in data.shape[0]:
-        redis.set(str(data.iloc[i, 1]), str(data.iloc[i, 2]))
+@app.get("/data/inputdata")
+async def inputdata():
+    need_to_load = pd.read_csv('./data/data.csv')
+    for i in range(10000):
+        print(i)
+        redis.set(str(need_to_load.iloc[i, 1]), str(need_to_load.iloc[i, 2]))
     return {"message": "Success."}
