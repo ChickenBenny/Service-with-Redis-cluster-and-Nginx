ef43cc3e0f116897b91a1c706375d3ca19b1c08f
ChickenBenny <zxc123benny14159@gmail.com>
2022-08-01 15:06:46 +0800

    change redis connect to cluster

--- app1/main.py
+++ app1/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from redis import Redis
+from rediscluster import RedisCluster
 
 app = FastAPI()
-redis = Redis(host='redis', port=6379)
+# redis = Redis(host='redis', port=6379)
+rc = RedisCluster(startup_nodes=[{"host": "redis-1", "port": "7001"}], decode_responses=True)
 
 @app.get("/")
 async def home():
@@ -10,8 +12,13 @@ async def home():
 
 @app.get("/convert/{target}")
 async def convert(target):
-    merchant = redis.get(target)
+    merchant = rc.get(target)
     if merchant:
         return {"Merchant": merchant}
     else:
         return {"Merchant": "None"}
+
+# @app.get("/test")
+# async def test():
+#     res = rc.get("ping")
+#     return {"res": res}
--- app2/main.py
+++ app2/main.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
 from redis import Redis
+from rediscluster import RedisCluster
+
 
 app = FastAPI()
-redis = Redis(host='redis', port=6379)
+rc = RedisCluster(startup_nodes=[{"host": "redis-1", "port": "7001"}], decode_responses=True)
+
 
 @app.get("/")
 async def home():
@@ -10,7 +13,7 @@ async def home():
 
 @app.get("/convert/{target}")
 async def convert(target):
-    merchant = redis.get(target)
+    merchant = rc.get(target)
     if merchant:
         return {"Merchant": merchant}
     else:
