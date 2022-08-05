a84412288d86ec9ca9b9545ff9d6fb84fbf1b451
ChickenBenny <zxc123benny14159@gmail.com>
2022-08-05 11:50:47 +0800

    add cluster node

--- app2/main.py
+++ app2/main.py
@@ -4,7 +4,12 @@ from rediscluster import RedisCluster
 
 
 app = FastAPI()
-rc = RedisCluster(startup_nodes=[{"host": "redis-1", "port": "7001"}], decode_responses=True)
+rc = RedisCluster(startup_nodes=[{"host": "redis-1", "port": "7001"},
+                                 {"host": "redis-2", "port": "7002"},
+                                 {"host": "redis-3", "port": "7003"},
+                                 {"host": "redis-4", "port": "7004"},
+                                 {"host": "redis-5", "port": "7005"},
+                                 {"host": "redis-6", "port": "7006"}], decode_responses=True)
 
 
 @app.get("/")
