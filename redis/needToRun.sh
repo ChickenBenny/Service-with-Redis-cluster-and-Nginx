25a6597ebd7b4d3b1fa13736cb2cd0427a742e9e
ChickenBenny <zxc123benny14159@gmail.com>
2022-08-15 15:17:53 +0800

    update bash

--- redis/needToRun.sh
+++ redis/needToRun.sh
@@ -1,4 +1,5 @@
-ip=172.28.140.146
+#!/bin/bash
+ip=172.28.129.121
 for port in {7001..7006}; 
 do 
 mkdir -p ./${port}/conf && PORT=${port} IP=${ip} envsubst < ./redis-cluster.tmpl > ./${port}/conf/redis.conf; 
